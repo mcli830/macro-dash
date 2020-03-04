b91baa9b3c7f5e1cec78ec9ba195603f8c20dd5c
mcli830 <39263631+mcli830@users.noreply.github.com>
2020-03-04 12:41:24 -0800

    add gapi sample data retrieval

--- src/App.js
+++ src/App.js
@@ -15,7 +15,32 @@ import Spinner from './components/spinner';
 
 function App(props) {
 
-  const { isSignedIn, loading, error, auth } = props;
+  const { isSignedIn, loading, error, auth, client } = props;
+
+  const [state, setState] = React.useState({
+    response: null,
+  })
+
+  const getSampleData = () => {
+    // handle incomplete load
+    if (!client) return null;
+    // get sample spreadsheet data
+    client.sheets.spreadsheets.values.get({
+      spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
+      range: 'Class Data!A2:E',
+    }).then(function(response) {
+      const range = response.result;
+      setState({
+        ...state,
+        response,
+      })
+    }, function(response) {
+      setState({
+        ...state,
+        response,
+      })
+    });
+  }
 
   return (
     <div className="App">
@@ -24,17 +49,25 @@ function App(props) {
           Macro Dash
         </div>
         {loading ? <Spinner /> : (
-          <div>
+          <div className="d-inline-flex align-items-center">
+            <button
+              className="btn btn-outline-success"
+              onClick={getSampleData}
+            >
+              Get Sample Data
+            </button>
+            <div className="mx-3">
+              <span> Status: </span>
+              <span className={`badge badge-${isSignedIn ? 'success' : 'light'}`}>
+                {isSignedIn ? 'ONLINE' : 'OFFLINE'}
+              </span>
+            </div>
             <button
               className={`btn btn-${isSignedIn ? 'outline-' : ''}primary`}
               onClick={isSignedIn ? auth.signOut : auth.signIn}
             >
               {isSignedIn ? 'Sign Out' : 'Login'}
             </button>
-            <span> Status: </span>
-            <span className={`badge badge-${isSignedIn ? 'success' : 'light'}`}>
-              {isSignedIn ? 'ONLINE' : 'OFFLINE'}
-            </span>
           </div>
         )}
       </nav>
@@ -44,11 +77,17 @@ function App(props) {
           <p>This is the main content</p>
           <hr className="my-4" />
           <p>App props:</p>
-          <pre className="alert alert-dark">
+          <pre className="alert alert-dark my-1">
             {JSON.stringify({
               loading: props.loading,
               error: props.error,
               isSignedIn: props.isSignedIn,
+            }, null, 2)}
+          </pre>
+          <p>Response:</p>
+          <pre className="alert alert-dark my-1">
+            {JSON.stringify({
+              response: state.response
             }, null, 2)}
           </pre>
         </div>
